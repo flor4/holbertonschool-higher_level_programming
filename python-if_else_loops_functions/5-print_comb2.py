9ef5e9e2b1a87d041039e96e477f2af9107779aa
flor4 <salanson.flora@outlook.fr>
2025-09-04 14:33:20 +0200

    fix: new line

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(100):
-    if i < 99:
-        print(f"{i:02d}, ", end="")
-    elif i == 99:
-        print(f"{i:02d}")
+for i in range(0, 99):
+    print("{:02}".format(i), end=", ")
+print(99)
