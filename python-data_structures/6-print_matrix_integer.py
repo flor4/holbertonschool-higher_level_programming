937af4ba13a98dc623ec03b6388b426ea8dde5b2
flor4 <salanson.flora@outlook.fr>
2025-09-09 11:09:41 +0200

    Add: a print() at the end

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+
     for row in matrix:
-        for i in range(len(row)):
-            if i < len(row) - 1:
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                print("{:d}".format(row[i]))
+        for index, element in enumerate(row):
+            print("{:d}".format(element), end="")
+
+            if index != len(row) - 1:
+                print(" ", end="")
+        print()
