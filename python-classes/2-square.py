5d8c6e3193483cf9fd8cedb11c7c6973c9f639b6
flor4 <salanson.flora@outlook.fr>
2025-09-16 22:01:02 +0200

    Fix: code error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,13 @@ class Square:
     """A square with a private size representation"""
 
     def __init__(self, size=0):
-
-        """Initialize a new Square with
+        """Initialize a new Square with a
         private size attribute"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
         self.__size = size
