561a77cbb8d7d34a8f3a8703c057710f38068d9b
flor4 <salanson.flora@outlook.fr>
2025-09-24 17:41:21 +0200

    Raise an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module based on 5-base-geometry"""
+
+
+class BaseGeometry:
+    """An class representing base geometry."""
+
+    def area(self):
+        """class base-geometry,
+        raise an Exception with the message area() is not implemented
+        """
+    raise Exception("area() is not implemented")
