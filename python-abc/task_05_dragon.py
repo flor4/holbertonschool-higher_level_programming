36f97f9adac54ecd7cf22950f2290d2b3f75a312
flor4 <salanson.flora@outlook.fr>
2025-09-26 22:43:39 +0200

    demonstrating mixins with a Dragon class that can swim and fly.

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module demonstrating mixins with a Dragon class that can swim and fly.
+"""
+
+
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
