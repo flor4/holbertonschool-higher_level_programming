deb24794850fe0e803c714ca2bf4db8cabd0d79a
flor4 <salanson.flora@outlook.fr>
2025-09-26 10:06:43 +0200

    Return: str() should return the square description

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Defines the BaseGeometry class with integer validation.
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    With size validated as a positive integer.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square with given the size of the square.
+        """
+        self.integer_validator("size", size)  # Validation de 'size'
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+        """
+
+        return f"[Square] {self.__size}/{self.__size}"
