2ef12462e95ca223b0642c3e9b0ea94e4059c680
flor4 <salanson.flora@outlook.fr>
2025-09-26 14:50:56 +0200

    Add: VerboseList with overridden list methods

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module defining VerboseList, a subclass of list with verbose notifications.
+"""
+
+
+class VerboseList(list):
+    """
+    A custom list that prints messages when items are added or removed.
+    """
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
