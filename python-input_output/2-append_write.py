b7a323f93bb722cc6beda80d89cf7ad578eaa859
flor4 <salanson.flora@outlook.fr>
2025-10-03 13:24:29 +0200

    pycodestyle: line to long

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,6 +7,7 @@ and returns the number of characters added.
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file and returns the number of characters added."""
+    """Appends a string at the end of a text file
+    and returns the number of characters added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
