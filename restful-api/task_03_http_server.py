b4db8c17f9ad3dc176c3af65f274ffaff35c4642
flor4 <salanson.flora@outlook.fr>
2025-10-07 15:16:05 +0200

    Add basic HTTP server with /, /data, /info endpoints and 404 handling

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,24 +1,24 @@
 import http.server
 import json
-from http.server import BaseHTTPRequestsHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class MyHandler(BaseHTTPRequestsHandler):
+
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
-            self.end_header()
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API")
 
         elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
-            self.end_header()
+            self.end_headers()
             data = {
-                "name": "John"
+                "name": "John",
                 "age": 30,
-                "city":"New York"
+                "city": "New York"
             }
             json_data = json.dumps(data)
             self.wfile.write(json_data.encode('utf-8'))
@@ -31,24 +31,28 @@ class MyHandler(BaseHTTPRequestsHandler):
 
         elif self.path == "/info":
             self.send_response(200)
-            self.send_header("Content-type"; "application/json")
-            self.end_header()
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
             info = {
                 "version": "1.0",
                 "description": "A simple API built with http.server"
             }
-        
+            json_data = json.dumps(info)
+            self.wfile.write(json_data.encode('utf-8'))
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Endpoint not found")
-    
-    def run():
-        server_adress = ('',8000)
-        httpq = HTTPServer(server_adress, MyHandler)
-        print("server running on http://localhost:8000...")
-        httpq.seve_forever()
 
-    if __name__ = "__main__":
-        run()
+
+def run():
+    server_address = ('', 8000)
+    httpq = HTTPServer(server_address, MyHandler)
+    print("Server running on http://localhost:8000...")
+    httpq.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
