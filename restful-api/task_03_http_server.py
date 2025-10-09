a7da17269cd4a4a3bc356a9902b342f27fbee000
flor4 <salanson.flora@outlook.fr>
2025-10-09 14:44:11 +0200

    fix: add bytes and comment

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,14 +2,23 @@ import http.server
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+"""
+Set up a basic web server using the http.server module.
+Handle different types of HTTP requests (GET, POST, etc.).
+Serve JSON data in response to specific endpoints.
+"""
+
 
 class MyHandler(BaseHTTPRequestHandler):
+    """ My handler"""
     def do_GET(self):
+        """return void"""
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write("Hello, this is a simple API".encode('utf-8'))
+            self.wfile.write
+            (bytes("Hello, this is a simple API".encode("utf-8")))
 
         elif self.path == "/data":
             self.send_response(200)
@@ -21,7 +30,7 @@ class MyHandler(BaseHTTPRequestHandler):
                 "city": "New York"
             }
             json_data = json.dumps(data)
-            self.wfile.write(json_data.encode('utf-8'))
+            self.wfile.write(bytes(json_data.encode('utf-8')))
 
         elif self.path == "/status":
             self.send_response(200)
@@ -38,16 +47,20 @@ class MyHandler(BaseHTTPRequestHandler):
                 "description": "A simple API built with http.server"
             }
             json_data = json.dumps(info)
-            self.wfile.write(json_data.encode('utf-8'))
+            self.wfile.write(bytes(json_data.encode('utf-8')))
 
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write("Endpoint not found".encode('utf-8'))
+            self.wfile.write(bytes("Endpoint not found".encode('utf-8')))
 
 
 def main():
+    """
+    main - Main function
+    Return: Void
+    """
     server_address = ('', 8000)
     httpd = HTTPServer(server_address, MyHandler)
     print("Server running on http://localhost:8000...")
