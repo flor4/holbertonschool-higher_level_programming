4d143033212a32df423ff00e4e6054c9054f45a1
flor4 <salanson.flora@outlook.fr>
2025-11-11 10:29:51 +0100

    Add a click event listener to the element

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+// Add a click event listener to the element with id "red_header"
+document.querySelector('#red_header').addEventListener('click', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});
